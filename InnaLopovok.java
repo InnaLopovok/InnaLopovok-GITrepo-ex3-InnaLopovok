bf71907a0d57d33b89a8fb92bf022dc3b06eb043
InnaLopovok <inka20010@gmail.com>
2022-03-07 14:09:26 +0200

    change number 5

--- InnaLopovok.java
+++ InnaLopovok.java
@@ -1,7 +1,7 @@
 public class InnaLopovok{
    public static void main(String[] args){
             for(int i=1;i<=10;i++) {
-			          System.out.println("Change number 4"+ i);
+			          System.out.println("Change number 5"+ i);
 		    }
 	}
 }	
